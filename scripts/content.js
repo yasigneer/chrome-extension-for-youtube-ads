c3085b1b6f48d7ca018f66a41d11442081abb0c8
yasinakarsu <yasin.akarsu@inomera.com>
2023-11-26 22:35:16 +0300

    initilaze project w/ manifest.json

---
+++ scripts/content.js
@@ -0,0 +1,26 @@
+document.querySelector('video').addEventListener('playing', playHandler, false);
+function playHandler(e) {
+    speedUpAndMuteAdVideo();
+    skipAd();
+}
+
+function speedUpAndMuteAdVideo(){
+    var adElement = document.getElementsByClassName('video-ads ytp-ad-module')[0],
+        style = window.getComputedStyle(adElement),
+        display = style.getPropertyValue('display');
+    if(display === 'block'){
+        document.querySelector('video').playbackRate = 16.0;
+        document.querySelector('video').muted = true;
+    }
+}
+
+
+function skipAd(){
+    if(isSkipAdButtonExist()){
+        document.querySelector(".ytp-ad-skip-button-modern").click();
+    }
+}
+
+function isSkipAdButtonExist(){
+    return document.body.contains(document.querySelector(".ytp-ad-skip-button-modern"));
+}
